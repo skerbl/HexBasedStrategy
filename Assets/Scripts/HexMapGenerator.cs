4be439ca506c04835198d7c934155a1a6a0d1428
skerbl <stephankerbl@gmail.com>
2020-09-27 21:08:38 +0200

    Made it possible to push landmasses away from the map edges. Map can be split into 1-4 continents.

--- Assets/Scripts/HexMapGenerator.cs
+++ Assets/Scripts/HexMapGenerator.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class HexMapGenerator : MonoBehaviour
 {
+    struct MapRegion
+    {
+        public int xMin, xMax, zMin, zMax;
+    }
+
     [SerializeField]
     HexGrid grid = default;
 
@@ -22,11 +27,17 @@ public class HexMapGenerator : MonoBehaviour
     [SerializeField, Range(20, 200)]
     int chunkSizeMax = 100;
 
-    [SerializeField, Range(0, 10), Tooltip("Restricts land chunk centers away from the edge of the map.")]
+    [SerializeField, Range(0, 10), Tooltip("Creates a border of water along the horizontal edges of the map.")]
     int mapBorderX = 5;
 
-    [SerializeField, Range(0, 10), Tooltip("Restricts land chunk centers away from the edge of the map.")]
+    [SerializeField, Range(0, 10), Tooltip("Creates a border of water along the horizontal edges of the map.")]
     int mapBorderZ = 5;
+
+    [SerializeField, Range(1, 4), Tooltip("Breaks the map up into continents.")]
+    int regionCount = 1;
+
+    [SerializeField, Range(0, 10), Tooltip("Separates the continents with water.")]
+    int regionBorder = 5;
 
     [SerializeField, Range(-4, 0)]
     int elevationMinimum = -2;
@@ -48,7 +59,7 @@ public class HexMapGenerator : MonoBehaviour
 
     private int cellCount;
     private int searchFrontierPhase;
-    int xMin, xMax, zMin, zMax;
+    private List<MapRegion> regions;
     private HexCellPriorityQueue searchFrontier;
 
     public void GenerateMap(int x, int z)
@@ -77,11 +88,7 @@ public class HexMapGenerator : MonoBehaviour
             grid.GetCell(i).WaterLevel = waterLevel;
         }
 
-        xMin = mapBorderX;
-        xMax = x - mapBorderX;
-        zMin = mapBorderZ;
-        zMax = z - mapBorderZ;
-
+        CreateRegions();
         CreateLand();
         SetTerrainType();
 
@@ -94,15 +101,120 @@ public class HexMapGenerator : MonoBehaviour
     }
 
     /// <summary>
+    /// Breaks the map up into 1-4 separate regions. Currently, they are
+    /// split along straight lines, equal in size, and equal in landmass.
+    /// Maybe add some more advance splitting methods that produce more organic results?
+    /// </summary>
+    void CreateRegions()
+    {
+        if (regions == null)
+        {
+            regions = new List<MapRegion>();
+        }
+        else
+        {
+            regions.Clear();
+        }
+
+        switch (regionCount)
+        {
+            default:
+                CreateOneRegion();
+                break;
+            case 2:
+                CreateTwoRegions();
+                break;
+            case 3:
+                CreateThreeRegions();
+                break;
+            case 4:
+                CreateFourRegions();
+                break;
+        }
+    }
+
+    private void CreateOneRegion()
+	{
+        MapRegion region;
+        region.xMin = mapBorderX;
+        region.xMax = grid.cellCountX - mapBorderX;
+        region.zMin = mapBorderZ;
+        region.zMax = grid.cellCountZ - mapBorderZ;
+        regions.Add(region);
+    }
+
+    private void CreateTwoRegions()
+	{
+        MapRegion region;
+        if (Random.value < 0.5f)
+        {
+            region.xMin = mapBorderX;
+            region.xMax = grid.cellCountX / 2 - regionBorder;
+            region.zMin = mapBorderZ;
+            region.zMax = grid.cellCountZ - mapBorderZ;
+            regions.Add(region);
+            region.xMin = grid.cellCountX / 2 + regionBorder;
+            region.xMax = grid.cellCountX - mapBorderX;
+            regions.Add(region);
+        }
+        else
+        {
+            region.xMin = mapBorderX;
+            region.xMax = grid.cellCountX - mapBorderX;
+            region.zMin = mapBorderZ;
+            region.zMax = grid.cellCountZ / 2 - regionBorder;
+            regions.Add(region);
+            region.zMin = grid.cellCountZ / 2 + regionBorder;
+            region.zMax = grid.cellCountZ - mapBorderZ;
+            regions.Add(region);
+        }
+    }
+
+    private void CreateThreeRegions()
+	{
+        MapRegion region;
+        region.xMin = mapBorderX;
+        region.xMax = grid.cellCountX / 3 - regionBorder;
+        region.zMin = mapBorderZ;
+        region.zMax = grid.cellCountZ - mapBorderZ;
+        regions.Add(region);
+        region.xMin = grid.cellCountX / 3 + regionBorder;
+        region.xMax = grid.cellCountX * 2 / 3 - regionBorder;
+        regions.Add(region);
+        region.xMin = grid.cellCountX * 2 / 3 + regionBorder;
+        region.xMax = grid.cellCountX - mapBorderX;
+        regions.Add(region);
+    }
+
+    private void CreateFourRegions()
+	{
+        MapRegion region;
+        region.xMin = mapBorderX;
+        region.xMax = grid.cellCountX / 2 - regionBorder;
+        region.zMin = mapBorderZ;
+        region.zMax = grid.cellCountZ / 2 - regionBorder;
+        regions.Add(region);
+        region.xMin = grid.cellCountX / 2 + regionBorder;
+        region.xMax = grid.cellCountX - mapBorderX;
+        regions.Add(region);
+        region.zMin = grid.cellCountZ / 2 + regionBorder;
+        region.zMax = grid.cellCountZ - mapBorderZ;
+        regions.Add(region);
+        region.xMin = mapBorderX;
+        region.xMax = grid.cellCountX / 2 - regionBorder;
+        regions.Add(region);
+    }
+
+    /// <summary>
     /// Raises a random chunk of land by one elevation step (with a chance of raising by 2).
     /// </summary>
     /// <param name="chunkSize">The size of the chunk of land to be raised</param>
     /// <param name="budget">The total number of cells that should be raised above the water level</param>
     /// <returns>The remaining amount of land that wasn't raised above the water level</returns>
-    int RaiseTerrain(int chunkSize, int budget)
+    int RaiseTerrain(int chunkSize, int budget, MapRegion region)
     {
         searchFrontierPhase += 1;
-        HexCell firstCell = GetRandomCell();
+        HexCell firstCell = GetRandomCell(region);
         firstCell.SearchPhase = searchFrontierPhase;
         firstCell.Distance = 0;
         firstCell.SearchHeuristic = 0;
@@ -156,10 +268,10 @@ public class HexMapGenerator : MonoBehaviour
     /// <param name="chunkSize">The size of the chunk of land to be lowered</param>
     /// <param name="budget">The total number of cells that should be lowered</param>
     /// <returns>The new amount of land that wasn't raised above the water level</returns>
-    int SinkTerrain(int chunkSize, int budget)
+    int SinkTerrain(int chunkSize, int budget, MapRegion region)
     {
         searchFrontierPhase += 1;
-        HexCell firstCell = GetRandomCell();
+        HexCell firstCell = GetRandomCell(region);
         firstCell.SearchPhase = searchFrontierPhase;
         firstCell.Distance = 0;
         firstCell.SearchHeuristic = 0;
@@ -209,16 +321,25 @@ public class HexMapGenerator : MonoBehaviour
     void CreateLand()
     {
         int landBudget = Mathf.RoundToInt(cellCount * landPercentage * 0.01f);
-        for (int guard = 0; landBudget > 0 && guard < 10000; guard++)
+        for (int failsafe = 0; failsafe < 10000; failsafe++)
         {
-            int chunkSize = Random.Range(chunkSizeMin, chunkSizeMax + 1);
-            if (Random.value < sinkProbability)
+            bool sink = Random.value < sinkProbability;
+            for (int i = 0; i < regions.Count; i++)
             {
-                landBudget = SinkTerrain(chunkSize, landBudget);
-            }
-            else
-            {
-                landBudget = RaiseTerrain(chunkSize, landBudget);
+                MapRegion region = regions[i];
+                int chunkSize = Random.Range(chunkSizeMin, chunkSizeMax - 1);
+                if (sink)
+                {
+                    landBudget = SinkTerrain(chunkSize, landBudget, region);
+                }
+                else
+                {
+                    landBudget = RaiseTerrain(chunkSize, landBudget, region);
+                    if (landBudget == 0)
+                    {
+                        return;
+                    }
+                }
             }
         }
 
@@ -240,8 +361,8 @@ public class HexMapGenerator : MonoBehaviour
         }
     }
 
-    HexCell GetRandomCell()
+    HexCell GetRandomCell(MapRegion region)
     {
-        return grid.GetCell(Random.Range(xMin, xMax), Random.Range(zMin, zMax));
+        return grid.GetCell(Random.Range(region.xMin, region.xMax), Random.Range(region.zMin, region.zMax));
     }
 }
